3deb0977bc6d3af86ce3c3ad82cf64c1f120ad72
paupalou <paupalou@gmail.com>
2021-04-04 11:25:25 +0200

    installer fixes

--- script/installers.sh
+++ script/installers.sh
@@ -82,7 +82,7 @@ function _install_item {
   local pre_hooks
   local post_hooks
   if [ -n "${!hooks}" ]; then
-    local num_hooks=$(echo "${!dependencys}" | wc -w)
+    local num_hooks=$(echo "${!hooks}" | wc -w)
     echo $num_hooks
     for hook in ${!hooks}; do
       pre_hooks=$(_get_value $hook "pre")
