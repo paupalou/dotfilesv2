74ff4c0c2c7d347f65422731d171f9325b5d321c
paupalou <paupalou@gmail.com>
2021-04-05 22:10:21 +0200

    installer fixes

--- script/installers.sh
+++ script/installers.sh
@@ -139,7 +139,7 @@ function _is_package_installed {
       version=$(_is_${type}_installed $package)
     fi
   else
-    local cmd=_run_command "$version_cmd"
+    local cmd=$(${version_cmd})
     version="$(echo $cmd | head -1 | grep -o -e '[0-9]\+\(\.[0-9]\+\)*' | head -1)"
   fi
 
