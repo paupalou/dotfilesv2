f0695d2a4945a994da688e673af2bd344998043c
paupalou <paupalou@gmail.com>
2021-04-10 16:47:49 +0200

    installer fixes

--- script/pkg.tar.zst.installer.from.pacman.sh
+++ script/pkg.tar.zst.installer.from.pacman.sh
@@ -5,10 +5,6 @@ function _install_pkg.tar.zst_from_pacman {
   local run=$5
   local is_package_dependency=$6
 
-  # if [ -n "$run" ]; then
-  #   _run_command "$run"
-  # fi
-
   print_installing $package_name "pacman" $is_package_dependency
   yes | sudo pacman -S $1 1>/dev/null 2>&1
   echo $(pc "  ✓" $green$bold)
